713433b09410a839f75e76969157f082ff7de501
babayaga01100 <babayaga01100@gmail.com>
2022-10-12 18:47:25 +0900

    four commit

--- lib/page/pregnant_graph_page.dart
+++ lib/page/pregnant_graph_page.dart
@@ -1,15 +1,11 @@
-
-// import 'dart:html';
-import 'dart:math';
 import 'package:dio/dio.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 late List<double> array_graph = List.filled(8, 0, growable: true);
-var int_list = List<int>.filled(5, 0);
-
+// late List<double> array_graph = [10,20,30,40,30,0,40];
+// var int_list = List<int>.filled(5, 0);
 
 class PregnantGraphPage extends StatefulWidget {
   static const routeName = '/camera-page';
@@ -24,103 +20,80 @@ class PregnantGraphPage extends StatefulWidget {
 
 class PregnantGraphPageState extends State<PregnantGraphPage> {
 
-  //late List<double> array_graph = [10, 20, 30, 40, 0, 40 ,25, 0];
-
-  List<Color> gradientColors = [
-    const Color(0xffffffff),
-    const Color(0xffffffff),
-  ];
-
-  bool showAvg = false;
-
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Column(
-            children: [
-              AspectRatio(aspectRatio: 3/2,
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10),
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("임신사 Graph Page")
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton.icon(
+                  onPressed: () { },
+                  icon: Icon(Icons.refresh), label: Text('새로고침')),
+              Text("교배",style:TextStyle(fontSize: 20),),
+              Column(
+                children: [
+                  AspectRatio(aspectRatio: 3/2,
+                    child:Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 30, 30),
+                      child: LineChart(
+                        mainChart_sow_cross(),
                       ),
-                      color: Color(0xffffffff)),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-
-                    // child: LineChart(
-                    //   mainChart(),
-                    // ),
-                  ),
-                ),
-              )
-            ],
-          ),
-          Column(
-            children: [
-              AspectRatio(aspectRatio: 3/2,
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10),
-                      ),
-                      color: Color(0xffffffff)),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-                    child: LineChart(
-                      mainChart1(),
                     ),
                   ),
-                ),
-              )
-            ],
-          ),
-          Column(
-            children: [
-              AspectRatio(aspectRatio: 3/2,
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10),
+                ],
+              ),
+              Column(
+                children: [
+                  Text("이유",style:TextStyle(fontSize: 20),),
+                  AspectRatio(aspectRatio: 3/2,
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 30, 30),
+                      child: LineChart(
+                        mainChart_sow_sevrer(),
                       ),
-                      color: Color(0xffffffff)),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-                    child: LineChart(
-                      mainChart2(),
                     ),
                   ),
-                ),
-              )
-            ],
-          ),
-          Column(
-            children: [
-              AspectRatio(aspectRatio: 3/2,
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10),
+                ],
+              ),
+              Column(
+                children: [
+                  Text("총산",style:TextStyle(fontSize: 20),),
+                  AspectRatio(aspectRatio: 3/2,
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 30, 30),
+                      child: LineChart(
+                        mainChart_total_baby(),
                       ),
-                      color: Color(0xffffffff)),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-                    child: LineChart(
-                      mainChart3(),
                     ),
                   ),
-                ),
-              )
+                ],
+              ),
+              Column(
+                children: [
+                  Text("포유",style:TextStyle(fontSize: 20),),
+                  AspectRatio(aspectRatio: 3/2,
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(20, 10, 30, 30),
+                      child: LineChart(
+                        mainChart_feed_baby(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
 
-//교배**********************
-LineChartData mainChart() {
+//교배복수**********************
+LineChartData mainChart_sow_cross() {
   List<Color> gradientColors_values = [
     const Color(0xff23b6e6),
     const Color(0xff02d39a),
@@ -130,18 +103,17 @@ LineChartData mainChart() {
     const Color(0xfffa0000),
     const Color(0xffffdd00),
   ];
-  // array_graph = receiveresult_graph();
+  print(array_graph);
   print("main 그래프 페이지 값 ");
-  // print(array_graph);
-  //var oneweek  = array_graph[0] as double;
+  print(array_graph);
   final oneweek_mate = array_graph[0];
-  final  twoweek_mate = array_graph[1];
+  final twoweek_mate = array_graph[1];
   final threeweek_mate = array_graph[2];
   final fourweek_mate = array_graph[3];
   final goal_mate = array_graph[4];
   final minvalue_mate = array_graph[5];
   final maxvalue_mate = array_graph[6];
-  final middlevalue_mate = array_graph[7];
+  // final middlevalue_mate = array_graph[7];
 
   return LineChartData(
 
@@ -209,10 +181,8 @@ LineChartData mainChart() {
           }else{
             return value.toInt().toString();
           }
-          return '';
+          // return '';
         },
-        reservedSize: 28,
-        margin: 12,
       ),
     ),
     // // borderData: FlBorderData(
@@ -258,9 +228,8 @@ LineChartData mainChart() {
   );
 }
 
-
-//이유******************
-LineChartData mainChart1() {
+//이유두수******************
+LineChartData mainChart_sow_sevrer() {
   List<Color> gradientColors_values = [
     const Color(0xff23b6e6),
     const Color(0xff02d39a),
@@ -281,7 +250,7 @@ LineChartData mainChart1() {
   final goal_teen = array_graph[4];
   final minvalue_teen = array_graph[5];
   final maxvalue_teen = array_graph[6];
-  final middlevalue_teen = array_graph[7];
+  // final middlevalue_teen = array_graph[7];
 
   return LineChartData(
 
@@ -349,10 +318,7 @@ LineChartData mainChart1() {
           }else{
             return value.toInt().toString();
           }
-          return '';
         },
-        reservedSize: 28,
-        margin: 12,
       ),
     ),
     // // borderData: FlBorderData(
@@ -398,8 +364,9 @@ LineChartData mainChart1() {
   );
 }
 
-//폐사******************
-LineChartData mainChart2() {
+//총산자수******************
+LineChartData mainChart_total_baby() {
+
   List<Color> gradientColors_values = [
     const Color(0xff23b6e6),
     const Color(0xff02d39a),
@@ -490,8 +457,7 @@ LineChartData mainChart2() {
           }
           return '';
         },
-        reservedSize: 28,
-        margin: 12,
+
       ),
     ),
     // // borderData: FlBorderData(
@@ -537,8 +503,8 @@ LineChartData mainChart2() {
   );
 }
 
-//출하******************
-LineChartData mainChart3() {
+//포유개시******************
+LineChartData mainChart_feed_baby() {
   List<Color> gradientColors_values = [
     const Color(0xff23b6e6),
     const Color(0xff02d39a),
@@ -550,16 +516,17 @@ LineChartData mainChart3() {
   ];
   // array_graph = receiveresult_graph();
   print("main 그래프 페이지 값 ");
+
   // print(array_graph);
   //var oneweek  = array_graph[0] as double;
-  final oneweek_shipment = array_graph[0];
-  final  twoweek_shipment = array_graph[1];
-  final threeweek_shipment = array_graph[2];
-  final fourweek_shipment = array_graph[3];
-  final goal_shipment = array_graph[4];
-  final minvalue_shipment = array_graph[5];
-  final maxvalue_shipment = array_graph[6];
-  final middlevalue_shipment = array_graph[7];
+  final oneweek_die = array_graph[0];
+  final  twoweek_die = array_graph[1];
+  final threeweek_die = array_graph[2];
+  final fourweek_die = array_graph[3];
+  final goal_die = array_graph[4];
+  final minvalue_die = array_graph[5];
+  final maxvalue_die = array_graph[6];
+
 
   return LineChartData(
 
@@ -569,12 +536,14 @@ LineChartData mainChart3() {
       horizontalInterval: 10,
       drawHorizontalLine: true,
 
+      //가로선
       getDrawingHorizontalLine: (value) {
         return FlLine(
           color: Color(0xff000000),
           strokeWidth: 1,
         );
       },
+      //세로선
       getDrawingVerticalLine: (value) {
         return FlLine(
           color: Color(0xff000000),
@@ -618,19 +587,17 @@ LineChartData mainChart3() {
         ),
         getTitles: (value) {
           // print('leftTitles $value');
-          if (value.toInt() == minvalue_shipment) {
-            return minvalue_shipment.toInt().toString();
-          }else if(value.toInt() == maxvalue_shipment){
-            return maxvalue_shipment.toInt().toString();
-          }else if(value.toInt() == goal_shipment){
-            return goal_shipment.toInt().toString();
+          if (value.toInt() == minvalue_die) {
+            return minvalue_die.toInt().toString();
+          }else if(value.toInt() == maxvalue_die){
+            return maxvalue_die.toInt().toString();
+          }else if(value.toInt() == goal_die){
+            return goal_die.toInt().toString();
           }else{
             return value.toInt().toString();
           }
           return '';
         },
-        reservedSize: 28,
-        margin: 12,
       ),
     ),
     // // borderData: FlBorderData(
@@ -641,18 +608,17 @@ LineChartData mainChart3() {
     minY: 0,
     maxY: 40,
     lineBarsData: [
-
       LineChartBarData(
         spots: [
-          FlSpot(0, oneweek_shipment),
-          FlSpot(3, twoweek_shipment),
-          FlSpot(6, threeweek_shipment),
-          FlSpot(9, fourweek_shipment),
+          FlSpot(0, oneweek_die),
+          FlSpot(3, twoweek_die),
+          FlSpot(6, threeweek_die),
+          FlSpot(9, fourweek_die),
         ],
 
         isCurved: true,
         colors: gradientColors_values,
-        barWidth: 100,
+        barWidth: 5,
         isStrokeCapRound: true,
         dotData: FlDotData(
           show: true,
@@ -660,10 +626,10 @@ LineChartData mainChart3() {
       ),
       LineChartBarData(
         spots: [
-          FlSpot(0, goal_shipment),
-          FlSpot(3, goal_shipment),
-          FlSpot(6, goal_shipment),
-          FlSpot(9, goal_shipment),
+          FlSpot(0, goal_die),
+          FlSpot(3, goal_die),
+          FlSpot(6, goal_die),
+          FlSpot(9, goal_die),
         ],
         isCurved: true,
         colors: gradientColors_avg,
@@ -676,24 +642,3 @@ LineChartData mainChart3() {
     ],
   );
 }
-//
-// Row(
-// children: [
-// print(a);
-// mainChart();
-// array_graph[0] = a[0].toDouble();
-// array_graph[1] = a[1].toDouble();
-// array_graph[2] = a[2].toDouble();
-// array_graph[3] = a[3].toDouble();
-// array_graph[4] = a[4].toDouble();
-// array_graph[5] = a[5].toDouble();
-// array_graph[6] = a[6].toDouble();
-// array_graph[7] = a[7].toDouble();
-// int_list[0] = a[4].toInt();
-// int_list[1] = a[6].toInt();
-//
-//
-// print(array_graph);
-// print(int_list);
-// ],
-// )
